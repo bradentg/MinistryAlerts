0da9456f043e0761a48cef038ee7a23bd2f7d36c
Braden Goodwin <6876787+BradenGoodwin@users.noreply.github.com>
2020-06-05 17:26:34 -0400

    Added GET request to wake up bot

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const client = new Discord.Client();
 const express = require('express');
 const app = express();
 
+app.get('/',(req,res) => {return res.send('Hello');});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
