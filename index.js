263563090cfaf029a75c0ea0d8aa1b0ccb1d843e
Braden Goodwin <goodwin.braden@gmail.com>
2020-06-04 21:45:20 -0400

    Bot v1

---
+++ index.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+client.once('ready', () => {
+  console.log('Ready!');
+});
+
+client.on('message', message => {
+  if (message.content === '!test') {
+    message.channel.send('This is a test. Do not be alarmed. Please carry on.');
+  } else if (message.content === '!hello') {
+    //guild = message.guild;
+    console.log(message.guild.name);
+    let guild = message.guild;
+    let member;
+    if (guild.available){
+      member = message.guild.member(message.author);
+    }
+
+    message.channel.send(`Greetings citizen No. ${message.author.id} (${member.nickname}).\nMay the Bean bless you, may the Ministry guide you, may the Emperor protect you.`);
+  }
+});
+
+client.login('NzE4MjU3MDczOTg0MzcyNzM3.XtmRrw.k9XGKeFQ9rKh4SoxD317qi8HiJw');
